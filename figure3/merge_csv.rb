60f9f869d605bf6078ac8ba3f60755ec4589acde
Tazro Inutano Ohta <inutano@gmail.com>
2019-05-21 10:47:39 +0900

    add comment header

--- figure3/merge_csv.rb
+++ figure3/merge_csv.rb
@@ -1,4 +1,10 @@
 #!/usr/bin/env ruby
+# Ruby script to merge csv files
+# License: MIT, Tazro Inutano Ohta, inutano@gmail.com
+#
+# Citation:
+# Ohta T, Kawashima T, Shinozaki NO, Dobashi A, Hiraoka S, Hoshino T, Kanno K, Kataoka T, Kawashima S, Matsui M, Nemoto W, Nishijima S, Suganuma N, Suzuki H, Taguchi Y, Takenaka Y, Tanigawa Y, Tsuneyoshi M, Yoshitake K, Sato Y, Yamashita R, Arakawa K, Iwasaki W. Collaborative environmental DNA sampling from petal surfaces of flowering cherry Cerasus × yedoensis “Somei-yoshino” across the Japanese archipelago. Journal of Plant Research [Internet]. 2018 Feb 19;131(4):709–17. Available from: http://dx.doi.org/10.1007/s10265-018-1017-x
+#
 # Usage:
 #  $ ruby merge_csv.rb ./data/*csv > ./data/assembled.csv
 
